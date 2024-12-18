eb2a3fb2f0eaf702afa63335372bf47d70442916
pavel.malinouski <pavelmalina96@gmail.com>
2024-12-18 14:36:48 +0300

    Initial commit

---
+++ src/main/java/com/malinouski/multitenant_app/MultitenantAppApplication.java
@@ -0,0 +1,13 @@
+package com.malinouski.multitenant_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultitenantAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/malinouski/multitenant_app/MultitenantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malinouski.multitenant_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
